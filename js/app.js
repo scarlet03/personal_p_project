e76ac4dc06ae9ed1f5e57092df2998430fa9dc08
ScarletSung <for_me09@naver.com>
2016-01-31 02:29:55 +0900

    remove all about maps in app.js and individual_page.html

--- js/app.js
+++ js/app.js
@@ -51,58 +51,6 @@ app.controller("Ctrl", function ($scope, $firebaseArray, $firebaseObject, $local
 		owner = $localstorage.get("authData")? JSON.parse($localstorage.get("authData")).uid : "";
 		//console.log("owner : " + owner);
 
-	// 	$scope.loadMap = function(){
-	//         var map, infoWindow;
-	//         var markers = [];
-	        
-	//         // map config
-	//         var mapOptions = {
-	//             center: new google.maps.LatLng(50, 2),
-	//             zoom: 4,
-	//             mapTypeId: google.maps.MapTypeId.ROADMAP,
-	//             scrollwheel: false
-	//         };
-	        
-	//         // init the map
-	//         function initMap() {
-	//             if (map === void 0) {
-	//                 map = new google.maps.Map(element[0], mapOptions);
-	//             }
-	//         }    
-	        
-	//         // place a marker
-	//         function setMarker(map, position, title, content) {
-	//             var marker;
-	//             var markerOptions = {
-	//                 position: position,
-	//                 map: map,
-	//                 title: title,
-	//                 icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png'
-	//             };
-
-	//             marker = new google.maps.Marker(markerOptions);
-	//             markers.push(marker); // add marker to array
-	            
-	//             google.maps.event.addListener(marker, 'click', function () {
-	//                 // close window if not undefined
-	//                 if (infoWindow !== void 0) {
-	//                     infoWindow.close();
-	//                 }
-	//                 // create new window
-	//                 var infoWindowOptions = {
-	//                     content: content
-	//                 };
-	//                 infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-	//                 infoWindow.open(map, marker);
-	//             });
-	//         }
-	        
-	//         // show the map and place some markers
-	//         initMap();
-	        
-	//         setMarker(map, new google.maps.LatLng($scope.maplatitude, $scope.maplongitude), 'London', 'Just some content');
-	// };
-
     $scope.getList = function() {
     	var echoRef = new Firebase(firebaseURL);
   		var query = echoRef.orderByChild("url");
@@ -262,30 +210,3 @@ app.controller("Ctrl", function ($scope, $firebaseArray, $firebaseObject, $local
     // load the list!
     $scope.getList();
 });
-
-// app.directive('googleMap',function(){
-// 	return{
-// 		restrict : 'E',
-// 		replace : true,
-// 		scope : {
-// 			accept : "&",
-// 			latitude : "@latitude",
-// 			longitude : "@longitude"
-// 		},
-// 		template : '<div id="gmaps"></div>',
-// 		controller : function($scope, $element, $attrs, $location){
-// 			// console.log("hello");
-// 			// $scope.save = function(){
-
-// 			// 	$scope.accept()
-// 			// };
-// 			$scope.accept()
-// 		}
-// 	}
-// });
-
-angular.module('appMaps', ['uiGmapgoogle-maps'])
-    .controller('mainCtrl', function($scope) {
-        $scope.map = {center: {latitude: 51.219053, longitude: 4.404418 }, zoom: 14 };
-        $scope.options = {scrollwheel: false};
-    });
